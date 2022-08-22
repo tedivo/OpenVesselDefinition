cca934dc755cd6c2663bf9170f67b1aab5d20b18
SntsDev <saants@saants.es>
2022-08-22 20:41:32 +0200

    Fix casing

--- src/converter/core/cleanup/cleanUpOVSJson.ts
+++ src/converter/core/cleanup/cleanUpOVSJson.ts
@@ -1,7 +1,5 @@
 import IOpenShipSpecV1 from "../../../models/v1/IOpenShipSpecV1";
-import cleanupStackInfo from "./cleanupStackInfo";
-
-// import cleanUpTierInfo from "./cleanUpTierInfo";
+import cleanupStackInfo from "./cleanUpStackInfo";
 
 export function cleanUpOVSJson(json: IOpenShipSpecV1) {
   const baylevelData = json.baysData;
