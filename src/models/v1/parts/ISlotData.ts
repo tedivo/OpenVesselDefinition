b2c9dc5fea6a74493dbb45c6d97dad08f812ceac
SntsDev <saants@saants.es>
2022-11-07 18:03:53 +0100

    Allow cones per size

--- src/models/v1/parts/ISlotData.ts
+++ src/models/v1/parts/ISlotData.ts
@@ -6,16 +6,38 @@ import {
 
 import { IDangerousAndHazardous } from "./IDangerousAndHazardous";
 import { TContainerLengths } from "./Types";
-import VoidTypesEnum from "../../base/enums/VoidTypesEnum";
 
-type IAcceptsContainers = Partial<{ [name in TContainerLengths]: TYesNo }>;
+/**
+ * This object, when present, has details per SIZE
+ */
+export interface ISlotSizeOptions {
+  /** Cone is required */
+  cone?: TYesNo;
+}
+
+/**
+ * Dictionary of sizes. Value can be 0/1 (1 means the size is allowed) or an object of type {@link ISlotSizeOptions}.
+ * When this later object is present it's equivalent to value 1 (the size is allowed).
+ */
+export type IAcceptsContainers = Partial<{
+  [name in TContainerLengths]: TYesNo | ISlotSizeOptions;
+}>;
+
 export default interface ISlotData {
-  /** Position: BAY_STACK_TIER */
+  /**
+   * @deprecated use pos.
+   * */
   position?: IIsoPositionPattern;
+  /** STACK_TIER. 2 chars for Row, 2 or 3 for Tier. i.e.: 0780 or 0014 or 00100 */
   pos: IJoinedRowTierPattern;
+  /** An object with sizes allowed of type {@link IAcceptsContainers} */
   sizes: IAcceptsContainers;
+  /** Reefer plug? */
   reefer?: TYesNo;
-  restricted?: VoidTypesEnum;
+  /** A slot without sizes can optionally be declared as restricted to signify the empty slot */
+  restricted?: TYesNo;
+  /** Cannot stow containers that need/are cool */
   coolStowProhibited?: TYesNo;
+  /** Cannot stow container that are dangerous */
   hazardousProhibited?: true | IDangerousAndHazardous;
 }
